ef2b1437bd541b0bb27cbf7a948c7df44b0c9e93
mark79-github <52877024+mark79-github@users.noreply.github.com>
2025-08-28 06:10:44 +0300

    Refactor ElectricityModel to be record

--- src/main/java/com/martinbg/electricityapi/model/ElectricityModel.java
+++ src/main/java/com/martinbg/electricityapi/model/ElectricityModel.java
@@ -1,25 +1,8 @@
 package com.martinbg.electricityapi.model;
 
-public class ElectricityModel {
-    private final ElectricityPrice electricityPrice;
-    private final LowVoltage lowVoltage;
-    private final HighVoltage highVoltage;
-
-    public ElectricityModel(ElectricityPrice electricityPrice, LowVoltage lowVoltage, HighVoltage highVoltage) {
-        this.electricityPrice = electricityPrice;
-        this.lowVoltage = lowVoltage;
-        this.highVoltage = highVoltage;
-    }
-
-    public ElectricityPrice getPrice() {
-        return electricityPrice;
-    }
-
-    public LowVoltage getLowVoltage() {
-        return lowVoltage;
-    }
-
-    public HighVoltage getHighVoltage() {
-        return highVoltage;
-    }
+public record ElectricityModel(
+        ElectricityPrice price,
+        LowVoltage lowVoltage,
+        HighVoltage highVoltage
+) {
 }
