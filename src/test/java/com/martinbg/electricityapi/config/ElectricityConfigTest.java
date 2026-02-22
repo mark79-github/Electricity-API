5918cbeab9c040a6ff2f62f2bafa9aeee3bf3a17
mark79-github <52877024+mark79-github@users.noreply.github.com>
2026-02-22 06:39:20 +0200

    Update tests to use BigDecimal for pricing and voltages

--- src/test/java/com/martinbg/electricityapi/config/ElectricityConfigTest.java
+++ src/test/java/com/martinbg/electricityapi/config/ElectricityConfigTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.math.BigDecimal;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
@@ -14,37 +16,31 @@ class ElectricityConfigTest {
 
     @Test
     void testGetDayPriceShouldReturnConfiguredValue() {
-        assertEquals(0.22, electricityConfig.getDayPrice(), 0.00001,
-                "The configured day price should be 0.22.");
+        assertEquals(BigDecimal.valueOf(0.22), electricityConfig.getDayPrice());
     }
 
     @Test
     void testGetNightPriceShouldReturnConfiguredValue() {
-        assertEquals(0.08000, electricityConfig.getNightPrice(), 0.00001,
-                "The configured night price should be 0.08000.");
+        assertEquals(BigDecimal.valueOf(0.08), electricityConfig.getNightPrice());
     }
 
     @Test
     void testGetLowVoltageTransmissionShouldReturnConfiguredValue() {
-        assertEquals(0.05000, electricityConfig.getLowVoltageTransmission(), 0.00001,
-                "The configured low voltage transmission should be 0.05000.");
+        assertEquals(BigDecimal.valueOf(0.05), electricityConfig.getLowVoltageTransmission());
     }
 
     @Test
     void testGetLowVoltageAccessShouldReturnConfiguredValue() {
-        assertEquals(0.01200, electricityConfig.getLowVoltageAccess(), 0.00001,
-                "The configured low voltage access should be 0.01200.");
+        assertEquals(BigDecimal.valueOf(0.01200), electricityConfig.getLowVoltageAccess());
     }
 
     @Test
     void testGetHighVoltageAccessShouldReturnConfiguredValue() {
-        assertEquals(0.01600, electricityConfig.getHighVoltageAccess(), 0.00001,
-                "The configured high voltage access should be 0.01600.");
+        assertEquals(BigDecimal.valueOf(0.016), electricityConfig.getHighVoltageAccess());
     }
 
     @Test
     void testGetHighVoltageTransmissionShouldReturnConfiguredValue() {
-        assertEquals(0.06000, electricityConfig.getHighVoltageTransmission(), 0.00001,
-                "The configured high voltage access should be 0.01600.");
+        assertEquals(BigDecimal.valueOf(0.06), electricityConfig.getHighVoltageTransmission());
     }
 }
